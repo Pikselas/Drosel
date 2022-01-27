984f09a873cfe05c9db87d31923dc2b2a5490eb4
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-01-27 17:22:19 +0530

    TemplateType converting handler and server to template type

--- Drosel/DroselServer.h
+++ Drosel/DroselServer.h
@@ -7,12 +7,57 @@
 #include"KsXstr.hpp"
 #include"Request.h"
 #include"Parser.h"
+
+template<class RequestT  = Request, class ResponseT = Response>
 class DroselServer
 {
 private:
 	std::unique_ptr<NetworkServer> server;
-	std::unordered_map < std::string, std::function<void(Request&, Response&)>> callables;
+	std::unordered_map < std::string, std::function<void(RequestT&, ResponseT&)>> PATH_FUNCTIONS;
 public:
-	void OnPath(const std::string& path, std::function<void(Request&, Response&)> callable);
+	void OnPath(const std::string& path, std::function<void(RequestT&, ResponseT&)> path_function);
 	void RunServer(const std::string& port);
 };
+
+
+template<class RequestT, class ResponseT>
+void DroselServer<RequestT, ResponseT>::OnPath(const std::string& path, std::function<void(RequestT&, ResponseT&)> path_function)
+{
+	PATH_FUNCTIONS[path] = path_function;
+}
+
+template<class RequestT, class ResponseT>
+void DroselServer < RequestT, ResponseT>::RunServer(const std::string& port)
+{
+	server = std::make_unique<NetworkServer>(port);
+	server->Listen();
+	while (true)
+	{
+		server->AcceptConnection();
+		auto rawData = server->Receive(1024);
+		if (rawData)
+		{
+			Parser ps(rawData.value().first, rawData.value().second);
+			auto path = ps.ParsePath();
+			Request request(std::move(ps.ParseHeaders()));
+			request.ClientIP = server->GetClientIP();
+			request.METHOD = ps.ParseRequestMethod();
+			request.GET = std::move(ps.ParsePathData());
+			/*std::thread([this, request]() {
+				Handler hnd (request, *server.get());
+				hnd(callables[request.path]);
+				}).detach();*/
+			Handler<RequestT, ResponseT> hnd(request,*server);
+			hnd(PATH_FUNCTIONS[path]);
+
+		}
+	}
+}
+
+template<class ...RequestTypes>
+class FinalRequestType : public RequestTypes...
+{};
+
+template<class ...ResponseTypes>
+class FinalResponseType : public ResponseTypes...
+{};
--- Drosel/Handler.h
+++ Drosel/Handler.h
@@ -5,20 +5,64 @@
 #include<functional>
 #include<sstream>
 #include<vector>
+template<class RequestT , class ResponseT>
 class Handler
 {
 private:
 	bool moved = false;
 private:
-	Request request;
-	Response response;
+	RequestT request;
+	ResponseT response;
 	std::vector<char> RAW_RESPONSE_DATA;
 private:
 	NetworkBuilder connection;
 public:
-	Handler(Request request, NetworkBuilder& server);
+	Handler(RequestT request, NetworkBuilder& server);
 	Handler(Handler&& handler) noexcept;
 	~Handler();
 public:
-	void operator()(std::function<void(Request&, Response&)> callable = nullptr);
+	void operator()(std::function<void(RequestT&, ResponseT&)> callable = nullptr);
 };
+
+template<class RequestT, class ResponseT>
+Handler<RequestT , ResponseT>::Handler(RequestT request, NetworkBuilder & conn) : request(request), connection(std::move(conn))
+{
+
+}
+
+template<class RequestT, class ResponseT>
+Handler<RequestT , ResponseT>::Handler(Handler&& handler) noexcept
+	:
+	request(handler.request), connection(std::move(handler.connection)), RAW_RESPONSE_DATA(std::move(handler.RAW_RESPONSE_DATA))
+{
+	moved = true;
+}
+
+template<class RequestT, class ResponseT>
+Handler<RequestT,ResponseT>::~Handler()
+{
+	if (!moved)
+	{
+		std::ostringstream ostr;
+		ostr << "HTTP/1.1 " << response.status_code << " " << Response::STATUS_CODE[response.status_code] << "\r\n";
+		ostr << response.headers.CounstructRaw();
+		ostr << "\r\n\r\n";
+		ostr << response.Get();
+		const auto& stremData = ostr.str();
+		std::copy(stremData.begin(), stremData.end(), std::back_inserter(RAW_RESPONSE_DATA));
+		connection.Send(RAW_RESPONSE_DATA.data(), RAW_RESPONSE_DATA.size());
+	}
+}
+
+template<class RequestT, class ResponseT>
+void Handler<RequestT , ResponseT>::operator()(std::function<void(RequestT&, ResponseT&)> callable)
+{
+	if (callable != nullptr)
+	{
+		callable(request, response);
+	}
+	else
+	{
+		response.status_code = 404;
+	}
+}
