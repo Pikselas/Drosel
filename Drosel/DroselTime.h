07073e714ab2dec7f488e6bd298b05674a4fd1c7
kashikichi <60090083+Pikselas@users.noreply.github.com>
2022-04-07 22:12:42 +0530

    basic Timeout and intervals

--- Drosel/DroselTime.h
+++ Drosel/DroselTime.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<chrono>
 #include<ctime>
+#include<thread>
 
 inline std::time_t Current_Time()
 {
@@ -15,4 +16,54 @@ inline std::string To_Complete_Date(const std::time_t seconds)
 	localtime_s(&t, &seconds);
 	std::strftime(buff, 64, "%a, %d-%b-%Y %H:%M:%S GMT" , &t);
 	return buff;
+}
+
+class time_session
+{
+    std::shared_ptr<bool> Continue;
+public:
+    void clear()
+    {
+        if (Continue.use_count() > 0)
+        {
+            *Continue = false;
+            Continue = nullptr;
+        }
+    }
+    friend time_session set_timeout(int ms, std::function<void()> f);
+    friend time_session set_interval(int ms, std::function<void()> f);
+};
+
+inline time_session set_timeout(int ms, std::function<void()> f)
+{
+    time_session session;
+    session.Continue = std::make_shared<bool>(true);
+    auto func = [](int time_out, std::shared_ptr<bool> s_ptr, auto func) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(time_out));
+        if (*s_ptr)
+        {
+            func();
+        }
+    };
+
+    std::thread(func, ms, session.Continue, f).detach();
+
+    return session;
+}
+
+inline time_session set_interval(int ms, std::function<void()> f)
+{
+    time_session session;
+    session.Continue = std::make_shared<bool>(true);
+    auto func = [](int time_gap, std::shared_ptr<bool> s_ptr, auto func)
+    {
+        bool Cont = false;
+        while (*s_ptr)
+        {
+            std::this_thread::sleep_for(std::chrono::microseconds(time_gap));
+            func();
+        }
+    };
+    std::thread(func, ms, session.Continue, f).detach();
+    return session;
 }
